77d47df80f0be15156cf21c265543e198a555416
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-05-27 15:47:54 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,11 +28,7 @@ export default function Home() {
               View Courses
             </span>
           </Link>
-          <Link href="/contact">
-            <span className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-6 rounded-full transition duration-300 cursor-pointer">
-              Contact for Mentorship
-            </span>
-          </Link>
+          
         </div>
       </section>
     </Layout>

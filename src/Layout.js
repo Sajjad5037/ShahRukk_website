f04c53c178a375f00604ca16a85fe2aceaa728ab
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-06-05 13:33:18 +0500

    Update Layout.js

--- src/Layout.js
+++ src/Layout.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Chatbot from './Chatbot'; // Adjust path as needed
+
 
 export default function Layout({ children }) {
   return (

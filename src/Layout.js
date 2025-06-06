fae0e043d64796ec9964ec2f8c1f90c87a4ddba1
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-06-06 22:40:19 +0500

    Update Layout.js

--- src/Layout.js
+++ src/Layout.js
@@ -46,7 +46,7 @@ export default function Layout({ children }) {
             <ul className="navbar-nav">
               {[
                 { label: "Home", href: "/" },
-                { label: "Essay Checker", href: "/essay_checker" },
+                { label: "Essay Checker", href: "https://class-management-system-new.web.app/" }
                 { label: "Reading Material", href: "/about" },
                 { label: "Attempted Questions", href: "/about" },
                 { label: "Time Table Maker", href: "/about" },
